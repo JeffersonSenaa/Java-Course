ba9a456c5d21ab39890a413fbc93923d345e654d
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-02-23 19:15:33 -0300

    Correção do Exercício e Ajuste no Repositorio

--- exercise_file/src/Main.java
+++ exercise_file/src/Main.java
@@ -1,16 +1,26 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String path = "/home/jefferson/ws-java/Java-Course/exercise_file/teste/source_file.csv";
-        String pathFolder = "/home/jefferson/ws-java/Java-Course/exercise_file/teste/";
-        String outPath = pathFolder + "summary.csv";
+        Scanner sc = new Scanner(System.in);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
+
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
+
+        boolean sucess = new File(sourceFolderStr + "/out").mkdir();
+
+        String targetFileStr = sourceFolderStr + "/out/summary.csv";        
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
             String line = br.readLine();
             while (line != null){
                 String[] data = line.split(",");
@@ -19,9 +29,10 @@ public class Main {
                 int items = Integer.parseInt(data[2]);
                 float amount = value*items;
 
-                try(BufferedWriter bw = new BufferedWriter(new FileWriter(outPath, true))){
+                try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr, true))){
                     bw.write(name + "," + Float.toString(amount));
                     bw.newLine();
+
                 }
                 catch(IOException e){
                     System.out.println(e.getMessage());
